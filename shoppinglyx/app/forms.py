64e3a38b0316be2d63fa0c4759f4e2cc11fcac53
seemakashyap20 <136772655+seemakashyap20@users.noreply.github.com>
2023-06-16 08:17:04 -0700

    estore

---
+++ shoppinglyx/app/forms.py
@@ -0,0 +1,78 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth.forms import UsernameField
+from django.contrib.auth.forms import PasswordChangeForm, PasswordResetForm
+from django.contrib.auth.forms import SetPasswordForm
+from django.contrib.auth.models import User
+from django.utils.translation import gettext,gettext_lazy as _
+from django.contrib.auth import password_validation
+from .models import  Customer
+
+class CustomerRegistrationForm(UserCreationForm):
+    password1 = forms.CharField(label='Password', widget=forms.
+    PasswordInput(attrs={'class':'form-control'})),
+    
+    password2 = forms.CharField(label='Confirm Password(again)', widget=forms.
+    PasswordInput(attrs={'class':'form-control'})),
+    
+    email = forms.CharField(required=True, widget=forms.
+    EmailInput(attrs={'class':'form-control'}))
+    
+    
+class Meta:
+    Model = User
+    fields = ['username','email','password1','password2']
+    label  = {'email':'Email'}    
+    widgets = {'username': forms.TextInput(attrs=
+     {'class': 'form-control'})}
+    
+#  login class    
+class  LoginForm(AuthenticationForm):
+     username = UsernameField(widget=forms.TextInput(attrs=
+     {'autofocus':True,'class':'form-control'}))
+     
+     password = forms.CharField(label=_("password"),strip=False,
+      widget=forms.PasswordInput(attrs=
+     {'autocomplete':'current-password','class':'form-control'}))
+     
+class MyPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label=_('Old Password'),  
+    strip=False, widget=forms.PasswordInput(attrs={'autocomplete': 
+    'current-password','autofocus':True,'class':'form-control'})) 
+    
+    new_password1 = forms.CharField(label=_('New Password'),  
+    strip=False, widget=forms.PasswordInput(attrs={'autocomplete': 
+    'new-password', 'class':'form-control'}),
+    help_text=password_validation.password_validators_help_text_html()) 
+     
+    new_password2 = forms.CharField(label=_(' Confirm New Password'),  
+    strip=False, widget=forms.PasswordInput(attrs={'autocomplete': 
+    'new-password', 'class':'form-control'}))
+    
+     
+class MyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(label=_('Email'),max_length=250,widget=forms.
+    EmailInput(attrs={'autocomplete':'email','class':'form-control'}))      
+    
+class MySetPasswordForm(SetPasswordForm):
+    new_passwprd1 = forms.CharField(label=_("New Password"),
+    strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password'
+    ,'class':'form-control'}),help_text=password_validation.
+    password_validators_help_text_html())
+    
+    new_passwprd2 = forms.CharField(label=_("Confirm New Password"),
+    strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password'
+    ,'class':'form-control'}))
+
+
+
+class CustomerProfileForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name','locality','city','state','zipcode']
+        widgets = {'name':forms.TextInput(attrs={'class':'form-control'}), 
+        'locality':forms.TextInput(attrs={'class':'form-control'}),
+        'city':forms.TextInput(attrs={'class':'form-control'}), 
+        'state':forms.Select(attrs={'class':'form-control'}),
+        'zipcode':forms.NumberInput(attrs={'class':'form-control'})}
